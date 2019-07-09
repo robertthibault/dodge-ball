7e55b630b8d29eda8f663f8c5a7e4f178242a601
robertthibault <robert.thibault33@gmail.com>
2019-07-09 17:54:43 +0200

    Add Script (spawn, hitbox, sound)

---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public GameObject hitbox;
+    public AudioClip brakeSound;
+    private AudioSource brakeAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        brakeAudioSource = gameObject.AddComponent<AudioSource>();
+        brakeAudioSource.clip = brakeSound;
+        brakeAudioSource.loop = false;
+        brakeAudioSource.volume = 1.0f;
+        brakeAudioSource.Stop();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject otherObj = collision.gameObject;
+        Debug.Log("Collided with: " + otherObj + ", -1pv");
+        brakeAudioSource.clip = brakeSound;
+        brakeAudioSource.Play();
+    }
+}
